e78c8215c4db9397bfdc5813f3a9426207d9836a
Дмитрий Кочеров <wolftmb@MacBook-Pro-Dmitrij.local>
2020-03-16 05:29:14 +0300

    added media

--- js/test.js
+++ js/test.js
@@ -1,16 +1,16 @@
-// let form = document.querySelector('.select-form'),
-// 	select = form.getElementsByTagName('input');
+let form = document.querySelector('.select-form'),
+	select = form.getElementsByTagName('input');
 
-// 	form.addEventListener('submit', function(event){
-// 		event.preventDefault();
+	form.addEventListener('submit', function(event){
+		event.preventDefault();
 
-// 		let request = new XMLHttpRequest();
-// 		request.open('POST', 'server.php');
-// 		request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+		let request = new XMLHttpRequest();
+		request.open('POST', 'server.php');
+		request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 
-// 		let formData = new FormData(form);
-// 		request.send(formData);
-// 	});
+		let formData = new FormData(form);
+		request.send(formData);
+	});
 
 	var app = new Vue({
         el: '#app',
